b8d4909e396617e5608092b15fae4aaaefa193c9
Zazzie Wharton <zazziewharton@gmail.com>
2026-07-09 00:38:20 +0100

    started on class for the app

--- script.js
+++ script.js
@@ -74,3 +74,24 @@ welcomeScreenOpen.addEventListener("click",
     function() {
         openWindow(welcomeScreen);
     });
+
+var selectedIcon = undefined
+
+function selectIcon(element) {
+    element.classList.add("selected");
+    selectedIcon = element
+}
+
+function deselectIcon(element) {
+    element.classList.remove("selected");
+    selectedIcon = undefined
+}
+
+function handleIconTap(element) {
+    if (element.classList.contains("selected")) {
+        deselectIcon(element)
+        openWindow(window)
+    } else {
+        selectIcon(element)
+    }
+}
