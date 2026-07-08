054956d475c6d6918a298c9072bdf4886f8eb252
Zazzie Wharton <zazziewharton@gmail.com>
2026-07-08 21:31:02 +0100

    moved timer into js file

---
+++ script.js
@@ -0,0 +1,10 @@
+<script>
+    function updateTime() {
+        var WHATNOW = new Date().toLocaleString();
+        var timeText = document.querySelector("#timeElement");
+        timeText.innerHTML = WHATNOW;
+    }
+
+    setInterval(updateTime, 1000);
+   
+</script>
