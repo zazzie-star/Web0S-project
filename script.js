1b4cffd5d16e35b3e0499eb867ef89710d25c30f
Zazzie Wharton <zazziewharton@gmail.com>
2026-07-08 22:03:15 +0100

    making window draggable using tut (hence few commits)

--- script.js
+++ script.js
@@ -6,3 +6,44 @@ function updateTime() {
 
 setInterval(updateTime, 1000);
    
+
+dragElement(document.getElementById("welcome"));
+
+function dragElement(element) {
+    var initialX = 0;
+    var initialY = 0;
+    var currentX = 0;
+    var currentY = 0;
+    
+    if (document.getElementById(element.welcome + "Welcome to ZazzieStar's ISS-WebOS!")) {
+        document.getElementById(element.welcome + "Welcome to ZazzieStar's ISS-WebOS!").onmousedown = startDragging;
+    } else {
+        element.onmousedown = startDragging
+    }
+    
+
+    function startDragging(e) {
+        e = e || window.event;
+        e.preventDefault();
+        initialX = e.clientX;
+        initialY = e.clientY;
+        document.onmouseup = stopDragging;
+        document.onmouseup = dragElement;
+    }
+
+    function dragElement(e) {
+        e = e || window.event;
+        e.preventDefault();
+        currentX = initialX - e.clientX;
+        currentY = initialY - e.clientY;
+        initialX = e.clientX;
+        initialY = e.clientY;
+        element.style.top = (element.offsetTop - currentY) + "px";
+        element.style.left = (element.offsetLeft - currentX) + "px";
+    }
+
+    function stopDragging() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
