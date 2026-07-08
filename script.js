32002f61e06fe9299ab45472f3c2d92340a41a0a
Zazzie Wharton <zazziewharton@gmail.com>
2026-07-08 22:26:39 +0100

    err i don't even remember made it drag added something to html where to drag froM??

--- script.js
+++ script.js
@@ -15,8 +15,8 @@ function dragElement(element) {
     var currentX = 0;
     var currentY = 0;
     
-    if (document.getElementById(element.id + "header")) {
-        document.getElementById(element.id + "header").onmousedown = startDragging;
+    if (document.getElementById(element.id + "welcomeheader")) {
+        document.getElementById(element.id + "welcomeheader").onmousedown = startDragging;
     } else {
         element.onmousedown = startDragging
     }
